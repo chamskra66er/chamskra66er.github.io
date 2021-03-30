ccf2dfd65ae4975853ec78dbdf0cae92adb91b52
blackgoodrabbit@gmail.com <blackgoodrabbit@gmail.com>
2021-03-30 14:06:24 +0300

    update session

--- js/session.js
+++ js/session.js
@@ -1,5 +1,32 @@
 // Сохранение данных в sessionStorage
-sessionStorage.setItem('setting', 'light');
+//sessionStorage.setItem('setting', 'light');
 
 // Получение данных из sessionStorage
 var data = sessionStorage.getItem('setting');
+var checkbox = document.querySelector("input[id=check]");
+
+if(data == null){
+	sessionStorage.setItem('setting', 'light');
+}
+
+if(data=='light'){
+	document.getElementById("check").checked = true;
+}
+else{
+	document.getElementById("check").checked = false;
+}
+
+
+
+checkbox.addEventListener('change', e => {
+  if (e.target.checked) {
+		sessionStorage.setItem('setting', 'light');
+		document.getElementById("check").checked = true;
+		console.log("Checkbox is checked..");
+  } 
+  else {
+		sessionStorage.setItem('setting', 'black');
+		document.getElementById("check").checked = false;
+		console.log("Checkbox is not checked..");
+	}
+});
