f512e5283fae6c2ca4ae9d8dd739203618f203d3
blackgoodrabbit@gmail.com <blackgoodrabbit@gmail.com>
2021-03-31 13:04:56 +0300

    added session3 & update skill

--- js/session3.js
+++ js/session3.js
@@ -4,24 +4,18 @@ var checkbox = document.querySelector("input[id=check]");
 
 function SetColorIndex(ckeck)
 {
-	var imgs = document.querySelectorAll('[id=proj]');
 	var about = document.querySelector('[id=about]');
 	var cham = document.getElementById('cham');
 	var foot = document.querySelector('[class=footer]');
 	
 	var head = document.querySelectorAll('[id=head1]');
-	var links = document.querySelectorAll('[class=ext]');
-	
-	//color for link cadetblue
 	
 	if(ckeck){
 		document.body.style.backgroundColor = "rgb(35,35,35)";	
 		
-		for(var i=0; i<imgs.length;i++)
+		for(var i=0; i<head.length;i++)
 		{
-			imgs[i].style.color = "azure";
-			head[i].style.color = "grey";
-			links[i].style.color = "cadetblue";
+			head[i].style.color = "azure";
 		}	
 		
 		about.style.color = "azure";
@@ -33,9 +27,7 @@ function SetColorIndex(ckeck)
 
 		for(var i=0; i<imgs.length;i++)
 		{
-			imgs[i].style.color = "black";
 			head[i].style.color = "black";
-			links[i].style.color = "black";
 		}	
 
 		about.style.color = "black";
